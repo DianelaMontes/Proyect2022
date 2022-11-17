da2ff9218a266314150e4a0855a982c9d9164e04
Mioporo <i201912833@cibertec.edu.pe>
2022-11-17 02:51:25 -0500

    connexion-mysql

---
+++ src/util/MySqlDbConexion.java
@@ -0,0 +1,42 @@
+package util;
+
+import java.sql.Connection;
+
+
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+
+
+
+public class MySqlDbConexion {
+	
+	
+	
+	
+	public static Connection getConexion()  {
+		Connection cn = null;
+		try {
+			
+		    Class.forName("com.mysql.jdbc.Driver");
+			cn=DriverManager.getConnection("jdbc:mysql://localhost/BD_PROYECTO_LP2","root","mysql");
+		} catch (SQLException e) {	
+			
+			e.printStackTrace();
+			
+			/*Agregacion opcional de panel de aviso de escepciones u errores*/
+			final JDialog dialog = new JDialog();
+			dialog.setAlwaysOnTop(true);
+			JOptionPane.showMessageDialog(dialog, "Error en la Conexion"+ e.getMessage());
+			/*---------------------------------------------------------------------------*/
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return cn;
+	}
+
+}
